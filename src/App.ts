572aa5dc83c4bdc806dd46b460d3ec5effc4b3e7
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-06-06 21:40:38 -0300

    Refactor App.ts to enhance user input handling and streamline user addition process

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,25 @@
+import readline from "readline";
 import { CreatedUser } from "./services/CreateUser";
 
-const userServices = new CreatedUser();
-const getAllUsers = userServices.getAllUsers();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
+const question = (str: string): Promise<string> => {
+  return new Promise((resolve) => rl.question(str, resolve));
+};
 
+async function main() {
+  const name = await question("Enter your name: ");
+  const email = await question("Enter your email: ");
+  const password = await question("Enter your password: ");
+
+  const userServices = new CreatedUser();
+  userServices.addUser(name, email, password);
+
+  console.log("Usuários cadastrados: ", await userServices.getAllUsers());
+  rl.close();
+}
+
+main();
