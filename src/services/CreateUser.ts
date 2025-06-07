ad1fcf820024fe5b0e259f35fc8f8783c08b9a2c
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-06-07 12:50:16 -0300

    Refactor addUser method in CreateUser.ts to use async/await and validate user input

--- src/services/CreateUser.ts
+++ src/services/CreateUser.ts
@@ -1,11 +1,28 @@
 import User from "../model/User";
 import { UserRepository } from "../repository/UserRepository";
+import { UserInputProps } from "../types/user.input";
+import { validateInputUser } from "../utils/validations/user/user.validation";
 
 export class CreatedUser {
   public userRepository: UserRepository = new UserRepository();
 
-  public addUser(name: string, email: string, password: string): any {
+  public async addUser(
+    name: string,
+    email: string,
+    password: string
+  ): Promise<UserInputProps | any> {
+    const isValid: boolean = await validateInputUser({
+      name,
+      email,
+      password,
+    });
+
+    if (!isValid) {
+      return Promise.reject(new Error("Invalid user input."));
+    }
+
     const user = new User(name, email, password);
+
     return this.userRepository.save(user);
   }
 
