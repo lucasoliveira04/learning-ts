117e20ce7787d269aab33671391ff80676cc7ede
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-06-08 12:40:36 -0300

    feat: add UserValidation class for validating user fields

---
+++ src/utils/validation/user.validation.ts
@@ -0,0 +1,17 @@
+import { UserRequest } from "../../types/user.request";
+import NameValidation from "./name.validation";
+
+class UserValidation {
+
+    public static validateFields(user: UserRequest): boolean {
+        const nameValidation = new NameValidation();
+
+        if (!nameValidation.validateField(user.name)){
+            throw new Error("Invalid name")
+        }
+
+        return true;
+    }
+}
+
+export default UserValidation;
