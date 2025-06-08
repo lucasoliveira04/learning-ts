eceb0df815c51ce40d68b35c3df7ec92068c1dcc
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-06-08 15:48:26 -0300

    refactor: simplify addUser method and ensure user creation is handled correctly

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,10 +1,12 @@
+import { User } from "../model/user.modal";
+import { userRepository } from "../repository/user.repository";
 import { UserRequest } from "../types/user.request";
 import UserValidation from "../utils/validation/user.validation";
 
 export class UserServices {
   constructor(private userValidator: typeof UserValidation) {}
 
-  public addUser(user: UserRequest): UserRequest {
+  addUser(user: UserRequest): UserRequest {
     try {
       this.userValidator.validateFields(user);
 
@@ -14,9 +16,21 @@ export class UserServices {
         updatedAt: new Date(),
       };
 
-      return userResponse;
+      const newUser = userRepository.create(userResponse);
+
+      userRepository.save(newUser);
+
+      return newUser;
     } catch (error) {
       throw error;
+    }
+  }
+
+  async getUsers(): Promise<User[]> {
+    try {
+      return await userRepository.find();
+    } catch (error) {
+      throw new Error("Failed to fetch users");
     }
   }
 }
