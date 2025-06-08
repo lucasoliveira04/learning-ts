3114c43d42946776ef44eb4083e938934ea297d2
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-06-08 15:48:02 -0300

    feat: initialize database connection with AppDataSource

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,19 @@
 import express, { Application } from "express";
 import routes from "./routes/app.routes";
+import { AppDataSource } from "./data-source";
 const app: Application = express();
 const PORT: number = 3000;
 
 app.use(express.json());
 app.use("/", routes);
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connection established successfully.");
+  })
+  .catch((error) => {
+    console.error("Error during Data Source initialization:", error);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
